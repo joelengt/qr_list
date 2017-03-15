372c3c03ff0e58da38df691346861a585b73831d
joelengt <joelengt@gmail.com>
2017-03-15 08:20:48 -0500

    Add basic files

---
+++ src/server/models/users/index.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose')
+
+var Schema = mongoose.Schema
+
+var userSchema = new Schema({
+    names:         { type: String },
+    last_names:    { type: String },
+    full_name:     { type: String },
+    photo:         {},
+    dni:           { type: String, unique: true },
+    email:         { type: String, unique: true },
+    username:      { type: String, unique: true },
+    password:      { type: String },
+    permiso:       { type: String },
+    token_auth:    { type: String },
+    fecha_creada:  { type: Date, default: Date.now },
+    status_connect: { type: Boolean, default: false }
+})
+
+
+var users = mongoose.model('users', userSchema)
+
+module.exports = users;
---
+++ src/server/routes/index.js
@@ -0,0 +1,13 @@
+
+function routes(app) {
+    var admin = require('./admin/index.js');
+    var users = require('./admin/users/index.js');
+    var items = require('./items/index.js');
+    
+    app.use('/info', admin);  // json, xml, request
+    app.use('/users', users);
+    app.use('/items', items);
+
+}
+
+module.exports = routes;
